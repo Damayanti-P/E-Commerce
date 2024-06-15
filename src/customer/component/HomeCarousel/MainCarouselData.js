b519f390ed3d94ff62a2a580625400333dba29d9
Damayanti-P <ponkshedamayanti@gmail.com>
2024-06-15 19:55:00 +0530

    add files

---
+++ src/customer/component/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,23 @@
+export const MainCarouselData=[
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_15.jpg",
+        path:"/women/clothing/lengha_choli"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_11.jpg",
+        path:"/women/clothing/women_saree"
+      
+    },
+    {
+        image:" https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_14.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_11.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_13.jpg",
+        path:"/women/clothing/Women_dress"
+    }
+]
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainCarousel from '../../component/HomeCarousel/MainCarousel'
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarousel/>
+
+      <div>Other Section</div>
+    </div>
+    
+  )
+}
+
+export default HomePage
